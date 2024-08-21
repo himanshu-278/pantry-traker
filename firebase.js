c7380087dfc1bfaadc4f4a7eaea09c741a769a1f
HIMANSHU KUMAR <104679475+himanshu-278@users.noreply.github.com>
2024-08-22 05:19:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoLJzf-KDg07KAGbBSGZ_YIBljgGeI6zY",
-  authDomain: "pantry-tracker-techbire.firebaseapp.com",
-  projectId: "pantry-tracker-techbire",
-  storageBucket: "pantry-tracker-techbire.appspot.com",
-  messagingSenderId: "541518585286",
-  appId: "1:541518585286:web:be57da7f89a346e115e6c7",
-  measurementId: "G-RS8SGCMGL1"
+  apiKey: "AIzaSyDX7CzcRwaYOnD1eQ8gBoJyr2kYdbJHlwk",
+  authDomain: "pantry-tracker-82426.firebaseapp.com",
+  databaseURL: "https://pantry-tracker-82426-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pantry-tracker-82426",
+  storageBucket: "pantry-tracker-82426.appspot.com",
+  messagingSenderId: "1073867150701",
+  appId: "1:1073867150701:web:ec5909fbf4720c1e3a7664",
+  measurementId: "G-P3H95TZ1G2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export { db };
