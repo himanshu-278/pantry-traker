cfd93cc2611353a4264cbd1812a7a85c4d4c4a87
HIMANSHU KUMAR <104679475+himanshu-278@users.noreply.github.com>
2024-08-22 05:46:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,3 @@
 npm install firebase 1
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDX7CzcRwaYOnD1eQ8gBoJyr2kYdbJHlwk",
-  authDomain: "pantry-tracker-82426.firebaseapp.com",
-  databaseURL: "https://pantry-tracker-82426-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "pantry-tracker-82426",
-  storageBucket: "pantry-tracker-82426.appspot.com",
-  messagingSenderId: "1073867150701",
-  appId: "1:1073867150701:web:ec5909fbf4720c1e3a7664",
-  measurementId: "G-P3H95TZ1G2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
